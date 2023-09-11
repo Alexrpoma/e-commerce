aaebacfbfe8a973349294110852899eebf29d48d
Alex Poma <alex.rpoma@outlook.com>
2023-09-11 18:24:22 -0400

    Customer services - getAll, getCustomer, createCustomer implemented.

---
+++ backend/customer/src/test/java/com/ecommerce/customer/services/CustomerServicesImpTest.java
@@ -0,0 +1,95 @@
+package com.ecommerce.customer.services;
+
+import com.ecommerce.customer.models.Customer;
+import com.ecommerce.customer.repositories.CustomerRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+class CustomerServicesImpTest {
+
+  @Mock
+  private CustomerRepository repository;
+  private AutoCloseable autoCloseable;
+  private CustomerService customerServiceUnderTest;
+
+  private Customer getCustomer() {
+    return Customer.builder()
+        .firstName("John")
+        .lastName("Doe")
+        .username("jonny")
+        .email("john@gmail.com")
+        .password("12313")
+        .build();
+  }
+
+  @BeforeEach
+  void setUp() {
+    autoCloseable = MockitoAnnotations.openMocks(this);
+    customerServiceUnderTest = new CustomerServicesImp(repository);
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    autoCloseable.close();
+  }
+
+  @Test
+  void itShouldGetAllCustomers() {
+    //when
+    customerServiceUnderTest.allCustomers();
+    //then
+    verify(repository).findAll();
+  }
+
+  @Test
+  void itShouldGetACustomer() {
+    //given
+    UUID uuid = UUID.randomUUID();
+    Customer customer = getCustomer();
+    customer.setUuid(uuid);
+    given(repository.findById(uuid)).willReturn(Optional.of(customer));
+    //when
+    customerServiceUnderTest.getCustomer(uuid);
+    //then
+    verify(repository, times(1)).findById(uuid);
+  }
+
+  @Test
+  void createCustomer() {
+    //given
+    Customer customer = getCustomer();
+    //when
+    when(repository.existCustomerByEmail(any())).thenReturn(false);
+    when(repository.existCustomerByUsername(any())).thenReturn(false);
+    when(repository.save(customer)).thenReturn(customer);
+    //then
+    Customer expect = customerServiceUnderTest.createCustomer(customer);
+    verify(repository, times(1)).save(customer);
+    assertNotNull(expect);
+    assertEquals(LocalDateTime.class, expect.getRegisteredAt().getClass());
+  }
+
+  @Test
+  @Disabled
+  void updateCustomer() {
+  }
+
+  @Test
+  @Disabled
+  void deleteCustomer() {
+  }
+}
