5c453e25cdfae211cd61fcba20f760fd112e7e10
Alex Poma <alex.rpoma@outlook.com>
2023-09-15 11:45:12 -0400

    createFraudCheckRecord to void

--- backend/customer/src/main/java/com/ecommerce/customer/client/FraudCheckHistoryClient.java
+++ backend/customer/src/main/java/com/ecommerce/customer/client/FraudCheckHistoryClient.java
@@ -10,5 +10,5 @@ import java.util.UUID;
 public interface FraudCheckHistoryClient {
 
   @PostMapping("/{customerId}")
-  FraudCheckHistory createFraudCheckRecord(@PathVariable("customerId") UUID customerId);
+  void createFraudCheckRecord(@PathVariable("customerId") UUID customerId);
 }
