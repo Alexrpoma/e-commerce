4e6abef82d93ba924a1d8c459188a6d3fa42aaa0
Alex Poma <alex.rpoma@outlook.com>
2023-09-18 18:58:42 -0400

    Dynamic path fraud service implemented with eureka server

---
+++ backend/customer/src/main/java/com/ecommerce/customer/utils/constants/Path.java
@@ -0,0 +1,7 @@
+package com.ecommerce.customer.utils.constants;
+
+public class Path {
+  public static final String API_FRAUD_CHECK = "/api/v1/fraud-check";
+  public static final String API_FRAUD_CHECK_CUSTOMER_ID = API_FRAUD_CHECK + "/{customerId}";
+  public static final String CUSTOMER_ID = "customerId";
+}
